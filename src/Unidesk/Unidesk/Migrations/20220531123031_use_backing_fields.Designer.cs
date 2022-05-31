2bea51b8693eb79434e97ad0cc571e730c4f0376
Jan Hybs <hybs.j@st-software.com>
2022-05-31 14:31:37 +0200

    feat: add backing fields migration

--- src/Unidesk/Unidesk/Migrations/20220531123031_use_backing_fields.Designer.cs
+++ src/Unidesk/Unidesk/Migrations/20220531123031_use_backing_fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Unidesk.Db;
 
@@ -11,9 +12,10 @@ using Unidesk.Db;
 namespace Unidesk.Migrations
 {
     [DbContext(typeof(UnideskDbContext))]
-    partial class UnideskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220531123031_use_backing_fields")]
+    partial class use_backing_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Unidesk/Unidesk/Migrations/20220531123031_use_backing_fields.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unidesk.Migrations
+{
+    public partial class use_backing_fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GrantsSerialized",
+                table: "UserRoles",
+                newName: "Grants");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Grants",
+                table: "UserRoles",
+                newName: "GrantsSerialized");
+        }
+    }
+}
