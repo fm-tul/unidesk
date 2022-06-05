1d3a8162d0496c6b0f6a7f9f4de062b7a68c01a6
Jan Hybs <hybs.j@st-software.com>
2022-06-05 15:43:12 +0200

    feat: :beers: add client project

---
+++ src/Unidesk/Unidesk/Server/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Unidesk.Db;
+
+namespace Unidesk.Server;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDevCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                {
+                    policy.WithOrigins("https://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .Build();
+                });
+        });
+
+        return services;
+    }
+
+    public static async Task<WebApplication> MigrateDbAsync(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<UnideskDbContext>();
+            await context.Database.MigrateAsync();
+            await context.SeedDbAsync();
+        }
+
+        return app;
+    }
+
+    public static WebApplication UseExceptionHandler(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+        else
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMigrationsEndPoint();
+        }
+
+        return app;
+    }
+}
