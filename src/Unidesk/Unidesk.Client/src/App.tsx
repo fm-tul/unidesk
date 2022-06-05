4708c6c26e91c34112bedc1a3dd44dd64d687f5f
Jan Hybs <hybs.j@st-software.com>
2022-06-05 18:23:06 +0200

    feat: add client example

--- src/Unidesk/Unidesk.Client/src/App.tsx
+++ src/Unidesk/Unidesk.Client/src/App.tsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from 'react'
 import { API_URL } from './core/config'
-
+import axios from 'axios'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    fetch(`${API_URL}/HelloWorld/api/helloworld`)
-      .then(data => data.text())
-      .then(text => setCount(text.length))
-  });
+  const authUser = async () => {
+    const response = await axios.post(`${API_URL}/api/Users/login`, {
+      username: 'admin',
+      password: 'admin'
+    },
+      { withCredentials: true })
+      ;
+    console.log(response.data);
+  }
+
+  const testUser = async () => {
+    const response = await axios.get(`${API_URL}/api/HelloWorld/helloworld`, { withCredentials: true });
+    console.log(response.data);
+  }
+
 
   return (
     <div className="App">
@@ -19,9 +29,15 @@ function App() {
         <h1 className="text-3xl font-bold underline bg-lime-300">
           Hello Vite + React + Tailwindcss!
         </h1>
-        <p>
+        <p className="flex flex-col">
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
+          </button>
+          <button type="button" onClick={authUser}>
+            authUser
+          </button>
+          <button type="button" onClick={testUser}>
+            testUser
           </button>
         </p>
       </header>
