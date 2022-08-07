3ae3d953e31cd68f28bb0410b0c1f957896624ac
Jan Hybs <hybs.j@st-software.com>
2022-08-07 17:29:21 +0200

    chore: add yup lib, edit prettier config, add path aliases

--- src/Unidesk/Unidesk.Client/prettier.config.js
+++ src/Unidesk/Unidesk.Client/prettier.config.js
@@ -1,4 +1,6 @@
 module.exports = {
     tailwindConfig: './tailwind.config.js',
     printWidth: 140,
+    bracketSpacing: true,
+    arrowParens: "avoid",
 }
--- src/Unidesk/Unidesk.Client/vite.config.ts
+++ src/Unidesk/Unidesk.Client/vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
-import checker from 'vite-plugin-checker'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    checker({ typescript: true }),
-    mkcert(),
-    react()
-  ]
-})
+  plugins: [tsconfigPaths(), checker({ typescript: true }), mkcert(), react()],
+});
